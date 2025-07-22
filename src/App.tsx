1996ae0ac98ba9367bcdfd48a4118c8afe23820f
Tristan Villamil <tristanvillamil6272003@gmail.com>
2025-07-22 11:23:52 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <div className="text-4xl text-green-500">Tailwind is working!</div>
+    </div>
+  );
+}
+
+export default App;
